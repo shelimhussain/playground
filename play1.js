54034db35c090c23c3a97f8f862c083604d88937
Md. Shelim Ekbal Hussain <salim436@gmail.com>
2020-08-24 20:36:15 +0530

    commit
    commit

--- play1.js
+++ play1.js
@@ -1,9 +1,46 @@
-const http = require('http');
+/* console.log("Hello from nodeJs!"); */
 
-const routes = require('./routes');
+const fs = require("fs");
 
-console.log(routes.someText);
+fs.writeFileSync("hello.txt", "Hello from Shelim NodeJs!");
 
-const server = http.createServer(routes.handler);
+const person = {
+    name: "Shelim",
+    age: 35,
+    greet(){
+        console.log("Hello my name is " + this.name)
+    }
+};
 
-server.listen(3000, function() { console.log("Listening on port 3000")});
+/* console.log(person);
+
+person.greet(); */
+
+const hobbies = ['Football', 'Cricket', 'Carrom'];
+
+//for (let hobby of hobbies){
+  //  console.log(hobby);
+//}
+
+/* console.log(hobbies);
+
+console.log(hobbies.map(hobby => "Hobby: " + hobby )); */
+
+/* Spread operator */
+
+const newArr = [...hobbies];
+
+/* console.log(newArr); */
+
+
+const newObj = {...person};
+
+console.log(newObj);
+
+/* Rest Operator */
+
+const toArr = (...arg) => {
+      return arg;
+}
+
+console.log(toArr(1,2,3,4));
