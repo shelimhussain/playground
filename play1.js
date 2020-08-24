823a0dfb5c5206332034b093be052236d7280086
Md. Shelim Ekbal Hussain <salim436@gmail.com>
2020-08-24 17:25:37 +0530

    Update play1.js
    Using import routes file

--- play1.js
+++ play1.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    
-});
+const routes = require('./routes');
+
+cosole.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000, function() { console.log("Listening on port 3000")});
