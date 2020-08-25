4a9c4d1df3975dcd36d71c1a41a8b2103e28d90e
Md. Shelim Ekbal Hussain <salim436@gmail.com>
2020-08-25 22:05:54 +0530

    commit assignment
    commit assignment

---
+++ nodeJs/assignment1/index-routes.js
@@ -0,0 +1,43 @@
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><h1>Welcome to nodeJs Assignment 1</h1><h3>Create User Form</h3><form action="/create-user" method="POST"><input type="text" name="username" placeholder="Enter username" /><button type="submit">Submit</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/users'){
+        res.setHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><h1>User List:</h1><ul><li>Shelim</li><li>Ekbal</li><li>Hussain</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url === '/create-user' && method === "POST"){
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parseBody = Buffer.concat(body).toString();
+            const username = parseBody.split("=")[1];
+            console.log(username);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();  
+        });
+    }
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>My NodeJs First page</title></head>');
+    res.write('<body><h1>Hello Shelim from NodeJs!</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+module.exports = requestHandler;
--- nodeJs/assignment1/index.js
+++ nodeJs/assignment1/index.js
@@ -1,2 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+
+const routes = require('./index-routes');
+
+const server = http.createServer(routes);
+
+server.listen(3000, function() { console.log("Listening on port 3000")});
